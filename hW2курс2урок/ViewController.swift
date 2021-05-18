25a1b486a06d143240878b51a38622e7810d3cf5
leha9321 <leha93.21@icloud.com>
2021-05-18 23:49:03 +0300

    work1

--- hW2курс2урок/ViewController.swift
+++ hW2курс2урок/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var windowColor: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+       // windowColor.backgroundColor = .red
+        
+        redSlider.value = 0.3
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .yellow
+        redSlider.maximumTrackTintColor = .red
+        redSlider.thumbTintColor = .blue
 
+        greenSlider.value = 0.3
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .yellow
+        greenSlider.maximumTrackTintColor = .red
+        greenSlider.thumbTintColor = .blue
 
+        blueSlider.value = 0.3
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .yellow
+        blueSlider.maximumTrackTintColor = .red
+        blueSlider.thumbTintColor = .blue
+        
 }
-
+  
+    @IBAction func red() {
+        windowColor.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    @IBAction func green() {
+        windowColor.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    @IBAction func blue() {
+        windowColor.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    
+//withAlphaComponent(CGFloat(redSlider.value))
+}
